9e4433d209055f5c73b80b8368b7b32280e5745c
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-03-12 18:52:39 -0300

    Some setup abou json

--- src/main/java/com/compremelhor/model/entity/EntityModel.java
+++ src/main/java/com/compremelhor/model/entity/EntityModel.java
@@ -9,7 +9,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.google.gson.Gson;
+
 @MappedSuperclass
+@JsonAutoDetect
 public abstract class EntityModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -19,10 +27,22 @@ public abstract class EntityModel implements Serializable {
 	public EntityModel() {}
 	
 	@Column(name = "date_created", nullable = false)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	protected LocalDateTime dateCreated;
 	
 	@Column(name = "last_updated", nullable = false)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	protected LocalDateTime lastUpdated;
+	
+	public EntityModel(String json) {
+		Gson g = new Gson();
+		EntityModel m = g.fromJson(json, EntityModel.class);
+		this.id = m.getId();
+		this.lastUpdated = m.getLastUpdated();
+		this.dateCreated = m.getDateCreated();
+	}
 	
 	public int getId() {
 		return id;
--- src/main/java/com/compremelhor/model/entity/Manufacturer.java
+++ src/main/java/com/compremelhor/model/entity/Manufacturer.java
@@ -1,6 +1,8 @@
 package com.compremelhor.model.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -8,13 +10,27 @@ import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.Gson;
+
 @Entity
 @Table(	name = "manufacturer",
 		uniqueConstraints = @UniqueConstraint(columnNames = {"id", "name"}))
+@JsonAutoDetect
 public class Manufacturer extends EntityModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public Manufacturer() {}
+	
+	@JsonCreator
+	public Manufacturer(String json) {
+		super(json);
+		Gson g = new Gson();
+		Manufacturer m = g.fromJson(json, Manufacturer.class);
+		this.name = m.getName();
+	}
 	
 	@NotNull @Size(max = 20)
 	private String name;
