01781d1a71e0fe1099a0a0dc8367b2ceaa4c2acd
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-07-10 17:31:39 -0300

    Fix equal and hashCode methods from Category and Manufacturer Entity

--- src/main/java/com/compremelhor/model/entity/Category.java
+++ src/main/java/com/compremelhor/model/entity/Category.java
@@ -1,6 +1,8 @@
 package com.compremelhor.model.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -12,9 +14,11 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "category", uniqueConstraints=@UniqueConstraint(columnNames = {"name", "id"}))
-public class Category extends EntityModel implements Serializable{
+public class Category extends EntityModel implements Serializable, Comparable<Category>{
 	private static final long serialVersionUID = 1L;
 
+	public Category() {}
+	
 	@NotNull @Size(max=45)
 	private String name;
 	
@@ -31,21 +35,40 @@ public class Category extends EntityModel implements Serializable{
 
 	@Override
 	public String toString() {
-		return "[id: " + id + ", name: " + name +"]";
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		return "[id : " + id 
+				+ ",name : " + name 
+				+ ",dateCreated : " + (dateCreated != null ? dateCreated.format(fmt) : null)
+				+ ",lastUpdated : " + (lastUpdated != null ? lastUpdated.format(fmt) : null) + "]";
 	}
-	
-	@Override
-	public int hashCode() {
-		return name != null ? name.hashCode() : -1;
-	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if (name == null || obj == null || ((Category)obj).getName() == null) return false;
-		
-		if (obj instanceof Category	&& ((Category)obj).getName().equals(name))
+		if (obj instanceof Category && this.id == (((Category)obj).getId()))
 			return true;
 		
 		return false;
 	}
+
+	@Override
+	public int compareTo(Category m) {
+		if (m == null || m.getName() == null || this.name == null) return -1;
+		return this.name.compareTo(m.getName());
+	}
+	
+	public static Category valueOf(String ca) {
+		String[] attrs = ca.substring(1, ca.length() -1).split(",");
+		Category m = new Category();
+		try {
+			m.setId(Integer.valueOf(attrs[0].split(" : ")[1]));
+			m.setName(attrs[1].split(" : ")[1]);
+			DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			m.setDateCreated(LocalDateTime.parse(attrs[2].split(" : ")[1], fmt));
+			m.setLastUpdated(LocalDateTime.parse(attrs[3].split(" : ")[1], fmt));
+		} catch (Exception e) { 
+			System.out.println(e.getMessage());
+		}
+		return m;
+	}
+
 }
--- src/main/java/com/compremelhor/model/entity/Manufacturer.java
+++ src/main/java/com/compremelhor/model/entity/Manufacturer.java
@@ -1,6 +1,8 @@
 package com.compremelhor.model.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -14,7 +16,7 @@ import com.google.gson.Gson;
 @Entity
 @Table(	name = "manufacturer",
 		uniqueConstraints = @UniqueConstraint(columnNames = {"id", "name"}))
-public class Manufacturer extends EntityModel implements Serializable {
+public class Manufacturer extends EntityModel implements Serializable, Comparable<Manufacturer> {
 	private static final long serialVersionUID = 1L;
 
 	public Manufacturer() {}
@@ -35,12 +37,41 @@ public class Manufacturer extends EntityModel implements Serializable {
 	public String getName() { return name; }
 
 	public void setName(String name) { this.name = name; }
-
+	
+	@Override
+	public boolean equals(Object m) {
+		if (m instanceof Manufacturer && this.id == ((Manufacturer)m).getId())
+			return true;
+		
+		return false;
+	}
+	
 	@Override
 	public String toString() {
-		return "Manufacturer[id: " + id 
-				+ ", name: " + name 
-				+ ", dateCreated: " + dateCreated
-				+ ", lastUpdated: " + lastUpdated +"]";
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		return "[id : " + id 
+				+ ",name : " + name 
+				+ ",dateCreated : " + (dateCreated != null ? dateCreated.format(fmt) : null)
+				+ ",lastUpdated : " + (lastUpdated != null ? lastUpdated.format(fmt) : null) + "]";
+	}
+	
+	public int compareTo(Manufacturer m) {
+		if (m == null || m.getName() == null || this.name == null) return -1;
+		return this.name.compareTo(m.getName());
+	}
+	
+	public static Manufacturer valueOf(String manufacturer) {
+		String[] attrs = manufacturer.substring(1, manufacturer.length() -1).split(",");
+		Manufacturer m = new Manufacturer();
+		try {
+			m.setId(Integer.valueOf(attrs[0].split(" : ")[1]));
+			m.setName(attrs[1].split(" : ")[1]);
+			DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			m.setDateCreated(LocalDateTime.parse(attrs[2].split(" : ")[1], fmt));
+			m.setLastUpdated(LocalDateTime.parse(attrs[3].split(" : ")[1], fmt));
+		} catch (Exception e) { 
+			System.out.println(e.getMessage());
+		}
+		return m;
 	}
 }
