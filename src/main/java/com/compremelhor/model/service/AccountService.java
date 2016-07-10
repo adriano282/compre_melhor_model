cc5eb9702ddee9fcd5bee28f31f4d9364647c7d7
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-07-10 17:33:38 -0300

    Implementation of find(Map<String, String>) method

--- src/main/java/com/compremelhor/model/service/AccountService.java
+++ src/main/java/com/compremelhor/model/service/AccountService.java
@@ -1,16 +1,32 @@
 package com.compremelhor.model.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import com.compremelhor.model.dao.AccountDao;
 import com.compremelhor.model.entity.Account;
 
 public class AccountService extends AbstractService<Account> {	
+	private static final long serialVersionUID = 1L;
 	
 	@Inject private AccountDao dao;
 		
 	public void setDao() { super.dao = this.dao;}
 	@Override 
 	protected void setStrategies() {}
+	
+	
+	@Override
+	public Account find(String attributeName, String attributeValue) {
+		if (attributeName == null || attributeValue == null)
+			return null;
+		
+		Map<String, Object> params = new HashMap<>();
+		params.put(attributeName, attributeValue);
+		
+		return dao.find(params);
+	}
 	
 }
